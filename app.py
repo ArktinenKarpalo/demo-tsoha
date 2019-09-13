77842a93846ab93928d8d578019fe0cffa277a25
ArktinenKarpalo <arktinenkarpalo@gmail.com>
2019-09-13 18:19:28 +0300

    Prepare for heroku deploy

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, url_for, redirect, render_template, request
 app = Flask("__name__")
 
-from routes import *
+from src.routes import *
 
 if __name__ == "__main__" :
     if app.config["ENV"] == "development" :
